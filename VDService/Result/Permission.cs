614b84682be3e58eeaafd480709273cad90f97c4
Vinson2003 <97678871+Vinson2003@users.noreply.github.com>
2023-11-09 09:01:46 +0700

    09-11-2023

--- VDService/Result/Permission.cs
+++ VDService/Result/Permission.cs
@@ -8,31 +8,31 @@ namespace VD.Service.Result
 {
 	public class PermissionList
 	{
-		public string Description { get; set; }
+		public string? Description { get; set; }
 		public int Seq { get; set; }
 		public int SubSeq { get; set; }
-		public string Display { get; set; }
+		public string? Display { get; set; }
 	}
 
 	public class PermissionGetRole
 	{
-		public string Role { get; set; }
-		public string Description { get; set; }
+		public string? Role { get; set; }
+		public string? Description { get; set; }
 		public int Seq { get; set; }
 		public int SubSeq { get; set; }
-		public string Display { get; set; }
+		public string? Display { get; set; }
 	}
 
 	public class ReqPermissionUpdate
 	{
-		public string Username { get; set; }
+		public string? Username { get; set; }
 		public long RoleId { get; set; }
 		public List<string> Permission { get; set; }
 	}
 
 	public class PermissionResult
 	{
-		public string Role { get; set; }
-		public string Description { get; set; }
+		public string? Role { get; set; }
+		public string? Description { get; set; }
 	}
 }
